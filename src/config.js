c3a72501b02ef0e39ce2effdc1bf78137befed18
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2024-10-06 22:02:04 -0400

    Added Config File

---
+++ src/config.js
@@ -0,0 +1,23 @@
+export default {
+  name: "Jake McQuade",
+  theme: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+  meta: {
+    title: "Jake McQuade - Entrepeneur. Developer. Designer.",
+    logo: "/favicon.ico",
+    description: "Entrepeneur. Developer. Designer.",
+    site: "https://jakemc.dev/",
+    siteName: "jakemc.dev",
+    keywords: ["Jake McQuade", "jakemc.dev", "McQuade", "Jake M", "jakemc"],
+  },
+  navItems: [{
+    label: "Home",
+    href: "/"
+  }, {
+    label: "Projects",
+    href: "/projects"
+  }],
+  projects: [],
+};
