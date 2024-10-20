6a390f7981d077a48da3415b0b41785a89089fcf
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2024-10-20 17:11:39 -0400

    Updated Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,16 @@
 import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
 import { ReactNode } from "react";
 
-import { ThemeProvider } from "../components/theme";
-import config from "../config";
+import { ThemeProvider } from "~/components/theme";
+import MouseEffect from "~/components/mouse";
+import Footer from "~/components/footer";
+import config from "~/config";
 
-import "~/styles/globals.css";
-export default async function RootLayout({ children }: Readonly<{ children: ReactNode; }>) {
-  return (
-    <html lang={"en"} data-theme={"dark"} style={{ colorScheme: "dark", scrollBehavior: "smooth" }} suppressHydrationWarning>
-      <head />
-      <body className={"bg-background min-h-screen antialiased"}>
-        <ThemeProvider children={children} />
-      </body>
-    </html>
-  );
-}
+const font = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const viewport: Viewport = { themeColor: "#323232" };
 export const generateMetadata = async (): Promise<Metadata> => {
@@ -23,41 +19,67 @@ export const generateMetadata = async (): Promise<Metadata> => {
 
   return {
     metadataBase: new URL("https://jakemc.dev"),
-    title, description,
+    title: {
+      default: title,
+      template: `%s | ${title}`,
+    },
+    description,
     keywords: config.meta.keywords,
     manifest: "/manifest.json",
     creator: "Jake McQuade (jakemc.dev)",
     publisher: "Jake McQuade (jakemc.dev)",
-    robots: "index, follow",
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-snippet": -1,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+      },
+    },
     appleWebApp: {
-      capable: true,
       title,
+      capable: true,
       startupImage: "/logo.png",
       statusBarStyle: "black-translucent",
     },
     openGraph: {
-      type: "website",
-      title, description,
+      siteName: config.meta.siteName,
+      images: config.meta.logo,
       url: config.meta.site,
-      images: config.meta.logo,
+      title,
+      description,
+      type: "website",
+      locale: "en_US",
     },
-    twitter: {
-      card: "summary",
-      site: "jakemc.dev",
-      title, description,
-      images: config.meta.logo,
-    },
+    twitter: { card: "summary", title },
     icons: {
       icon: [{ url: "/icon.png" }],
       shortcut: ["/favicon.ico"],
-      apple: [
-        { url: "/apple-icon.png" },
-        { url: "/apple-icon-x3.png", sizes: "180x180", type: "image/png" },
+      apple: [{ url: "/apple-icon.png" }, { url: "/apple-icon-x3.png", sizes: "180x180", type: "image/png" }],
+      other: [
+        {
+          rel: "apple-touch-icon-precomposed",
+          url: "/apple-touch-icon-precomposed.png",
+        },
       ],
-      other: [{
-        rel: "apple-touch-icon-precomposed",
-        url: "/apple-touch-icon-precomposed.png",
-      }],
     },
   };
 };
+
+import "~/styles/globals.css";
+export default async function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang={"en"} data-theme={"dark"} style={{ colorScheme: "dark", scrollBehavior: "smooth" }} suppressHydrationWarning>
+      <head />
+      <body className={"mx-auto min-h-screen max-w-2xl bg-background font-sans antialiased"}>
+        <ThemeProvider attribute={"data-theme"} defaultTheme={"dark"} disableTransitionOnChange>
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
