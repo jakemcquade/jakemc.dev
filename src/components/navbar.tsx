99614e0427a1e105aba52428fc29f2fe85db4b2f
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2024-10-20 20:12:07 -0400

    Better Icon Style

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -78,8 +78,8 @@ export function ModeToggle() {
 
   return (
     <Button variant={"ghost"} size={"icon"} onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-      <LuSun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <LuMoon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <LuSun className="absolute h-4 w-4 scale-0 transition-all dark:scale-100" />
+      <LuMoon className="h-4 w-4 scale-100 transition-all dark:scale-0" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
