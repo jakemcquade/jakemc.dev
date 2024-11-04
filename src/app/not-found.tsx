29731d12e97261fc9d4ec8ffcd6677a4e1632d03
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2024-11-04 16:27:56 -0500

    Redesigned 404 Page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,25 @@
 import NextImage from "next/image";
 import Link from "next/link";
+import { Button } from "~/components/button";
 
 export default function Render() {
   return (
-    <>
-      <div style={{ display: "flex", flexDirection: "column", width: "100%", height: "70vh", paddingBottom: "7rem" }}>
-        <div style={{ margin: "auto", height: "137.5px" }}>
-          <NextImage src={"/images/svgs/404.svg"} width={275} height={275} alt={"404 Image"} />
-        </div>
-        <div style={{ margin: "auto", textAlign: "center" }}>
-          <h1 style={{ fontWeight: "600", fontSize: "xx-large" }}>404 - Not Found</h1>
-          <p>Sorry, we couldn't find what you were looking for.</p>
-          <Link href={"/"}>Home</Link>
+    <div className={"flex flex-col h-[75dvh] gap-1 relative"}>
+      <div className={"h-full w-full m-auto text-center items-center justify-center flex absolute bottom-[25%] z-[-1] inset-0 overflow-visible"}>
+        <p className={"text-[14rem] sm:text-[25rem] font-extrabold h-fit w-fit opacity-[0.03] m-auto"}>404</p>
+      </div>
+      <div className={"flex flex-col gap-1 h-fit text-center m-auto z-50"}>
+        <h1 className={"text-3xl font-semibold"}>404 - Not Found</h1>
+        <p>Sorry, we couldn't find what you were looking for.</p>
+        <div className={"flex flex-row gap-2 justify-center"}>
+          <Link href={"/"} passHref>
+            <Button className={""}>Home</Button>
+          </Link>
+          <Link href={"/"} passHref>
+            <Button className={""}>Home</Button>
+          </Link>
         </div>
       </div>
-    </>
-  );
+    </div>
+  )
 }
