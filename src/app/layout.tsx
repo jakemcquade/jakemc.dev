4a0e971e117915ff3c0e99ab6348c4b0f1934e59
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2024-11-03 21:41:37 -0500

    Imported Module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
@@ -76,6 +77,7 @@ export default function RootLayout({ children }: Readonly<{ children: ReactNode 
           {children}
           <Footer />
         </ThemeProvider>
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
