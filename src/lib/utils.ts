e79ced2ef2c0db166c8073a018f0596d38a2dd2b
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2025-04-27 15:05:04 -0400

    Better Util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,50 @@
-import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { ClassValue, Options } from "~/types";
 
 export const getBaseUrl = () => process.env.NEXT_PUBLIC_SITE ?? "http://localhost:3000";
 export const getCanonicalUrl = (...pages: string[]) => `${getBaseUrl()}/${pages.join("/")}`;
+
+export const clsx = (...args: ClassValue[]) => args.map(flattenClass).filter(Boolean).join(" ");
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export function cva(base: string, options: Options) {
+	return function (props: { [key: string]: string | undefined }) {
+		const { variants, defaultVariants } = options;
+		let classes = base;
+
+		for (const variant in variants) {
+			const value = props[variant] || defaultVariants[variant];
+			if (value && variants[variant]?.[value]) {
+				classes += ` ${variants[variant]?.[value]}`;
+			}
+		}
+
+		if (props.className) {
+			classes += ` ${props.className}`;
+		}
+
+		return classes;
+	};
+}
+
+function flattenClass(mix: ClassValue): string | number {
+	if (typeof mix === "string" || typeof mix === "number") {
+		return mix;
+	}
+
+	if (Array.isArray(mix)) {
+		return mix.map(flattenClass).filter(Boolean).join(" ");
+	}
+
+	if (typeof mix === "object" && mix !== null) {
+		return Object.keys(mix)
+			.filter(key => mix[key])
+			.join(" ");
+	}
+
+	return "";
+}
+
 export function formatDate(date: string) {
   const currentDate = new Date().getTime();
   if (!date.includes("T")) date = `${date}T00:00:00`;
