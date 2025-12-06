83096acfe5edb67f8ffc4c417f6c16e2373ec6c6
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2025-12-06 13:12:32 -0500

    Updated Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import("tailwindcss").Config} */
-module.exports = {
+export default {
   darkMode: ["class", "[data-theme=\"dark\"]"],
   content: [
     "./src/components/**/*.{ts,tsx}",
